1952490b7ac38fd47720c6f594ae7d3f71a6c56a
nguyenphucluan <58660258+nguyenphucluan@users.noreply.github.com>
2022-06-04 19:57:09 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var loginRouter = require('./routes/login');
+var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin')
+const expressHandlebars =require('express-handlebars')
+
+var app = express();
+
+// view engine setup
+var secret = require('./secret.js')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var mongoConnect = require('./mongoDB/mongoconnect');
+app.use(require('cookie-parser')(secret.cookieSecret));
+app.use(require("express-session")(secret.sessionSecret));
+
+app.use('/', loginRouter);
+app.use('/index', usersRouter);
+app.use('/admin', adminRouter);
+mongoConnect.connectDB();
+
+//create a flash message
+app.use((req,res,next)=>{
+  res.locals.flash = req.session.flash
+  delete req.session.flash
+  next();
+}) 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
